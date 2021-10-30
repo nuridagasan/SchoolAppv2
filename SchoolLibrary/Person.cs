1669fc6e4763e27f7fb4d4f7633d7a138c72c322
dagasannuri <dagasannuri@gmail.com>
2021-10-30 16:34:02 +0100

    3 new classes are added

---
+++ SchoolLibrary/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    //abstract class 
+    // it does not instantiate directly from person class
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
